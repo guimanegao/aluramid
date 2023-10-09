b4c32a84b1bc2777dec28fe2a57a7facef3ad71c
guimanegao <129763307+guimanegao@users.noreply.github.com>
2023-10-09 08:41:41 -0300

    Add files via upload

---
+++ kauan/console-input-2023-9-18_8-52-33.js
@@ -0,0 +1,51 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+  
+    if (elemento === null) {
+      alert('Elemento nao encontrado');
+    }
+  
+    if (elemento != null && elemento.localName === 'audio' ) {
+          elemento.play();
+     }   
+     else {
+        // alert('Elemento nao encontrado ou seletor invalido');
+        console.log('Elemento não encontrado ou seletor invalido');
+     }    
+  }
+  
+  
+  const ListaDeTeclas = document.querySelectorAll('.tecla');
+  
+  let contador = 0;
+  
+  
+  for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+  
+     const tecla = ListaDeTeclas[contador];
+     const instrumento = tecla.classList[1];
+  
+     const idAudio = `#som_${instrumento}`;
+  
+     console.log(idAudio);
+  
+     tecla.onclick = function () {
+        tocaSom(idAudio);
+     }
+  
+     tecla.onkeydown = function (evento) { 
+  
+        console.log(evento.code == 'Space')
+  
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+          tecla.classList.add('ativa');
+        }
+  
+     
+     }   
+     tecla.onkeyup = function ()  {
+        tecla.classList.remove ('ativa');
+  
+     }
+  
+  } 
